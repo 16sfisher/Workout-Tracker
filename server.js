2a809f1ab2eba8f37e67de12ed9216c0d3945e65
Sawyer Fisher <sawyerkove@gmail.com>
2020-04-21 13:44:57 -0500

    rearranged

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(morgan('dev'));
+const app = express();
 
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
-//MONGODB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
-//Routes
-require("./routes/html-routes")(app);
-require("./routes/api-routes")(app);
+// routes
+app.use(require("./routes/api.js"));
 
-
-//Starts server listening on PORT
-app.listen(PORT, function(){
-    console.log(`App listening on Port ${PORT}!`);
-}); 
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
